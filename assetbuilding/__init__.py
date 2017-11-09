5a9fa5ed06e555b28905bdde46197719f6b547b4
Tim Fechner <tim.fechner@mailbox.org>
2017-11-09 10:12:38 +0100

    Add assetbuilding app

---
+++ assetbuilding/__init__.py
@@ -0,0 +1,6 @@
+import logging
+
+
+logger = logging.getLogger('assetbuilding')
+
+default_app_config = "assetbuilding.apps.AssetbuildingConfig"
---
+++ assetbuilding/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AssetbuildingConfig(AppConfig):
+    name = 'assetbuilding'
